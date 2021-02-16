e30ae7cc3068b6162f336dc6e6b1bdc44fdfb5db
Stephen Turner <vustephen@gmail.com>
2021-02-16 11:31:51 -0500

    initial commit

---
+++ R/skater-package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+# Quiet CRAN check notes for unbound global variables
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
