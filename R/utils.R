9da0b7b31402ceee8752657c854b5d1d35064329
Stephen Turner <vustephen@gmail.com>
2021-02-20 15:39:06 -0500

    initial work on infer degree function #16 not working

--- R/utils.R
+++ R/utils.R
@@ -11,11 +11,12 @@
 #'
 #' @return A tibble containing the degree, expected kinship coefficient (`k`),
 #'   lower (`l`) and upper (`u`) inference bounds for that degree.
-#' @export
 #'
 #' @examples
 #' dibble(3)
 #' dibble(10)
+#'
+#' @export
 dibble <- function(max_degree=3L) {
   stopifnot(is.numeric(max_degree))
   max_degree <- as.integer(round(max_degree))
@@ -27,4 +28,28 @@ dibble <- function(max_degree=3L) {
     dplyr::mutate(l=ifelse(dplyr::row_number()==dplyr::n(), -1, l)) %>%
     dplyr::mutate(degree=ifelse(dplyr::row_number()==dplyr::n(), NA_integer_, degree)) %>%
     dplyr::mutate(k=ifelse(dplyr::row_number()==dplyr::n(), 0, k))
+}
+
+
+#' Infer degree for a kinship coefficient
+#'
+#' @param k Kinship coefficient (numeric, typically between 0 and .5, although KING can produce values <0).
+#' @param degree_resolution The maximum degree relationship you want to infer.
+#'   Defaults to third-degree relatives. Anything lower than `degree_resolution`
+#'   will produce `NA` (i.e., unrelated). See [dibble][skater::dibble].
+#'
+#' @return Inferred degree, up to the `degree_resolution` (anything more distant is `NA`, i.e., unrelated).
+#'
+#' @examples
+#' infer_degree(0.0625)
+#' infer_degree(0.125)
+#' infer_degree(0.25)
+#' infer_degree(0.5)
+#'
+#' @export
+infer_degree <- function(k, degree_resolution=3L) {
+  stopifnot(is.numeric(k))
+  stopifnot(k %>% dplyr::between(-1, 1))
+  d <- dibble(degree_resolution)
+  as.integer(d$degree[which(purrr::map2_lgl(d$l, d$u, ~dplyr::between(k, .x, .y)))])
 }
