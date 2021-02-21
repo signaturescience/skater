9ffb62ce82a6fc76776fdb19be02292c16c7eb4d
Stephen Turner <vustephen@gmail.com>
2021-02-21 06:11:21 -0500

    test dibble #18

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,6 +1,14 @@
 library(skater)
 library(testthat)
 
+test_that("Degree tibble", {
+  expect_equal(3:9 %>% purrr::map(dibble) %>% purrr::map_int(nrow), (3:9)+2)
+  expect_error(dibble(12))
+  expect_warning(dibble(10))
+  expect_error(dibble(0))
+  expect_warning(dibble(2))
+})
+
 test_that("Degree inference from kinship coefficient", {
   expect_equal(kin2degree(.25), 1L)
   expect_equal(kin2degree(.125), 2L)
