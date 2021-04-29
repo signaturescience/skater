9c8eb4c56614c921b092be36c91e1c6096cc3228
VP Nagraj <pnagraj@signaturescience.com>
2021-04-29 12:56:52 -0500

    first pass at including ibd utils and relatedness functions

---
+++ R/ibd.R
@@ -0,0 +1,129 @@
+#' Compute kinship coefficient from IBD segments
+#'
+#' @description
+#' This function is used to retrieve a relatedness measure from IBD segments. The relatedness value returned is the kinship coefficent.
+#'
+#' The input data should be pairwise IBD segments prepared via \link[skater]{read_ibd}. The function will internally loop over each chromosome, and use a specified genetic map to convert shared segments to genetic units. After doing so, the function converts the shared length to a kinship coefficient.
+#'
+#' @param .ibd_data Tibble with IBD segments created using the \link[skater]{read_ibd} function
+#' @param .map Tibble with the genetic map data created using the \link[skater]{read_map} function
+#'
+#' @return Tibble with three columns:
+#'
+#' 1. id1 (sample identifier 1)
+#' 2. id2 (sample identifier 2)
+#' 3. kinship (kinship coefficent derived from shared segments)
+#'
+#' @export
+#'
+#' @references http://faculty.washington.edu/sguy/ibd_relatedness.html
+#'
+ibd2kin <- function(.ibd_data, .map) {
+
+  ## get the min base pair for each chr
+  ibd_min_bp <-
+    .ibd_data %>%
+    dplyr::group_by(chr) %>%
+    dplyr::summarise(bp = min(start), .groups = "drop")
+
+  ## get the max base pair for each chromsome
+  ibd_max_bp <-
+    .ibd_data  %>%
+    dplyr::group_by(chr) %>%
+    dplyr::summarise(bp = max(end), .groups = "drop")
+
+  ## loop over each chromosome to get length
+  ## this will be summed up and serve as denominator for kinship coefficient calculations
+  totalchromlength <- vector()
+  for(i in 1:length(unique(ibd_min_bp$chr))) {
+
+    chrom <- unique(ibd_min_bp$chr)[i]
+
+    ## these steps just get the min/max bp and map data for the given chromosome
+    tmp_ibd_min_bp <-
+      ibd_min_bp %>%
+      dplyr::filter(chr == chrom)
+
+    tmp_ibd_max_bp <-
+      ibd_max_bp %>%
+      dplyr::filter(chr == chrom)
+
+    tmp_chromgpos <-
+      .map %>%
+      dplyr::filter(chr == chrom)
+
+    ## use interpolate to get centimorgan lengths
+    mincm <- interpolate(ibd_bp = tmp_ibd_min_bp$bp, chromgpos = tmp_chromgpos)
+    maxcm <- interpolate(ibd_bp = tmp_ibd_max_bp$bp, chromgpos = tmp_chromgpos)
+
+    ## create chrom length and append to vector
+    tmp_totalchromlength <- maxcm - mincm
+    totalchromlength <- c(totalchromlength, tmp_totalchromlength)
+  }
+
+  ## use ibd data and total chromlength as denominator above to get the kinship value
+  .ibd_data %>%
+    dplyr::group_by(id1,id2) %>%
+    dplyr::summarise(totallength = sum(length), .groups = "drop") %>%
+    dplyr::mutate(kinship = totallength/(4*sum(totalchromlength))) %>%
+    dplyr::select(id1,id2,kinship)
+}
+
+#' Interpolate over segments
+#'
+#' This is an unexported helper used in in \link[skater]{ibd2kin}. The function interpolates over segments to apply genetic length to the segment. It is inspired by Python code distributed by the Browning lab ([documentation](http://faculty.washington.edu/sguy/ibd_relatedness.html)).
+#'
+#' @param ibd_bp Base pair for the IBD segment over which to interpolate
+#' @param chromgpos Genetic map data for a specific chromosome
+#'
+#' @return Numeric vector with the genetic distance shared at the segment.
+#'
+#' @references http://faculty.washington.edu/sguy/ibd_relatedness.html
+interpolate <- function(ibd_bp, chromgpos) {
+
+  tmp_set1 <-
+    chromgpos %>%
+    dplyr::filter(bp < ibd_bp)
+  tmp_set2 <-
+    chromgpos %>%
+    dplyr::filter(bp > ibd_bp)
+
+  if(nrow(tmp_set1) > 0 && nrow(tmp_set2) > 0) {
+    tmp_bp1 <- max(tmp_set1$bp)
+    tmp_bp2 <- min(tmp_set2$bp)
+  } else if (nrow(tmp_set1) > 0) {
+    tmp_bp2 <- max(tmp_set1$bp)
+    tmp_set1 <-
+      tmp_set1 %>%
+      dplyr::filter(bp != tmp_bp2)
+    tmp_bp1 <- max(tmp_set1$bp)
+  } else {
+    tmp_bp1 <- min(tmp_set2$bp)
+    tmp_set2 <-
+      tmp_set2 %>%
+      dplyr::filter(bp != tmp_bp1)
+    tmp_bp2 <- min(tmp_set2$bp)
+  }
+
+  if (tmp_bp1 == tmp_bp2) {
+    res <-
+      chromgpos %>%
+      dplyr::filter(bp == tmp_bp1) %>%
+      dplyr::pull(value)
+    return(res)
+  } else {
+
+    len1 <-
+      chromgpos %>%
+      dplyr::filter(bp == tmp_bp1) %>%
+      dplyr::pull(value)
+
+    len2 <-
+      chromgpos %>%
+      dplyr::filter(bp == tmp_bp2) %>%
+      dplyr::pull(value)
+
+    res <- len1+(len2-len1)*(ibd_bp - tmp_bp1)/(tmp_bp2-tmp_bp1)
+    return(res)
+  }
+}
