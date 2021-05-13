e80f1a9149d41a83c64c1da295d1bfba20eb7a85
Stephen Turner <vustephen@gmail.com>
2021-05-13 09:09:19 -0400

    tests for faster degree inference #16

--- tests/testthat/test-skater.R
+++ tests/testthat/test-skater.R
@@ -14,7 +14,7 @@ test_that("Degree inference from kinship coefficient", {
   expect_equal(kin2degree(.125), 2L)
   expect_equal(kin2degree(.0625), 3L)
   expect_identical(kin2degree(0), NA_integer_)
-  expect_identical(kin2degree(c(.5, .25, .125, .0625, .03125)), c(0L, 1L, 2L, 3L, NA))
+  expect_identical(kin2degree(c(.5, .25, .125, .0625, .03125, 0, .25, .5)), c(0L, 1L, 2L, 3L, NA, NA, 1L, 0L))
 })
 
 test_that("Expect errors on on plot_pedigree()", {
